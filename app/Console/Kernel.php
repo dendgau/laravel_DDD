a3aad6481e4c4354d9542751128fc253f586668b
Duy Tran <trankhanhduy201@gmail.com>
2020-10-11 16:50:34 +0700

    Fix schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('log:cron')->hourly();
+        $schedule->command('log:cron')->everyMinute();
     }
 
     /**
