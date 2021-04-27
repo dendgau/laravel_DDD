0a63131c2edf46c243bb60a9ab2675ca937545e2
Duy Tran <trankhanhduy201@gmail.com>
2021-04-27 17:03:37 +0700

    Remove config

--- config/ebays.php
+++ config/ebays.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'RuName' => 'Fulfillment-Fulfillm-Fulfil-mkcbpti',
-    'refreshToken' => 'v^1.1#i^1#r^1#p^3#f^0#I^3#t^Ul4xMF82OjExQTU0M0NBNzc2NjlGRTJBQTA3MUNCQUZDMUUxNERBXzFfMSNFXjEyODQ=',
+    'RuName' => '',
+    'refreshToken' => '',
     'header' => [
         'requestLanguage' => 'en-US',
         'authorization' => '',
